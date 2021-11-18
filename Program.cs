0092f0a2a16382c28ccdb8d8b173b9115b6b8a43
aaronhnash <77747939+aaronhnash@users.noreply.github.com>
2021-11-18 13:14:24 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro_to_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
